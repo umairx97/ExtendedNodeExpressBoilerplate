1347f746a9ed80d699c941760f09e9c465d1b18f
umairx97 <umair.bajwa@hotmail.com>
2019-09-10 21:08:30 +0500

    deploying to heroku

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,18 @@ exports = module.exports = function (app, mongoose) {
 
   /* GET users listing. */
   router.get('/', function (req, res, next) {
-    let User = new app.db.models.User({ name: "Umair", email: "umairahmedbajwa97@gmail.com" });
-    User.save().then(data => {
-      res.send(data);
-    });
+    // let User = new app.db.models.User({ name: "Umair", email: "umairahmedbajwa97@gmail.com" });
+    // User.save().then(data => {
+    //   res.send(data);
+    // });
+
+    const users = [
+      { name: "Umair", age: 22, profession: "Fullstack Developer and DevOps Engineer" },
+      { name: "Asad", age: 23, profession: "MERN Developer" },
+      { name: "Shahbaz", age: 21, profession: "React Native Developer" }
+    ]
+
+    res.send(users)
   });
 
   app.use('/user', router);
